d805acdff369546c6c11f049b89951eed5540074
jin <lj7812@gmail.com>
2020-07-05 18:51:56 +0900

    # 1.3 Understanding Functional Component, Props (3)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
 import React from "react";
 
+const FoodILike = [
+  {
+    name: "kimchi",
+    image:
+      "https://cdn.crowdpic.net/detail-thumb/thumb_d_AAA4C5E8C1E29791413BFBFF5419BA73.jpg",
+  },
+  {
+    name: "ramen",
+    image: "https://t1.daumcdn.net/cfile/tistory/035F8240519F7EF70C",
+  },
+  {
+    name: "samgiopsal",
+    image:
+      "https://cphoto.asiae.co.kr/listimglink/6/2017122807215214567_1514413315.jpg",
+  },
+  {
+    name: "jjuggumi",
+    image:
+      "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F2763B2415847A02C03",
+  },
+];
+
 function Food(props) {
   console.log(props);
-  return <h1> I Like {props.fav} ~_~</h1>;
+  return (
+    <div>
+      <h2>I Like {props.name}</h2>
+      <img src={props.picture} width="500" height="300" />
+    </div>
+  );
 }
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi</h1>
-
-      {/* name="kinchi" => HTML 과 같은 형태인데, JSX도 이런 문법을 사용함 : props (properties) */}
-      <Food fav="pizza" />
-      <Food fav="ramen" />
-      <Food fav="samgiopsal" />
-      <Food fav="jjuggumi" />
+      {FoodILike.map((dish) => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
