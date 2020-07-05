7491d82b9a9f436a782551a4cc73c1fb0ee55b73
jin <lj7812@gmail.com>
2020-07-05 20:26:55 +0900

    # 2.0 Class Component, State
    클래스 컴포넌트와 State에 대한 연습 예제

--- src/App.js
+++ src/App.js
@@ -1,48 +1,29 @@
 import React from "react";
 
-const FoodILike = [
-  {
-    id: 0,
-    name: "kimchi",
-    image:
-      "https://cdn.crowdpic.net/detail-thumb/thumb_d_AAA4C5E8C1E29791413BFBFF5419BA73.jpg",
-  },
-  {
-    id: 1,
-    name: "ramen",
-    image: "https://t1.daumcdn.net/cfile/tistory/035F8240519F7EF70C",
-  },
-  {
-    id: 2,
-    name: "samgiopsal",
-    image:
-      "https://cphoto.asiae.co.kr/listimglink/6/2017122807215214567_1514413315.jpg",
-  },
-  {
-    id: 3,
-    name: "jjuggumi",
-    image:
-      "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F2763B2415847A02C03",
-  },
-];
+class App extends React.Component {
+  state = {
+    count: 3,
+  };
 
-function Food({ name, picture }) {
-  return (
-    <div>
-      <h2>I Like {name}</h2>
-      <img src={picture} alt={name} width="500" height="300" />
-    </div>
-  );
-}
+  add = () => {
+    console.log("add");
+    this.setState({ count: this.state.count + 1 });
+  };
 
-function App() {
-  return (
-    <div className="App">
-      {FoodILike.map((dish) => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} />
-      ))}
-    </div>
-  );
+  minus = () => {
+    console.log("minus");
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>My state is : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
