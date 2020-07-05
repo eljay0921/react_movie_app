cdd4409b3a81a090a46d12b8acadd3fb433f2862
jin <lj7812@gmail.com>
2020-07-05 18:30:20 +0900

    # 1.3 Understanding Functional Component, Props
    JSX, Functional Component, Props를 이해하기 위한 예제

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
-import Potato from "./Potato";
+
+function Food(props) {
+  console.log(props);
+  return <h1> I Like Potato ~_~</h1>;
+}
 
 function App() {
   return (
     <div className="App">
       <h1>Hi</h1>
-      <Potato />
+
+      {/* name="kinchi" => HTML 과 같은 형태인데, JSX도 이런 문법을 사용함 : props (properties) */}
+      <Food
+        fav="kimchi"
+        something={true}
+        papapap={["hi", "bye", 1, 2, 3, false]}
+      />
     </div>
   );
 }
