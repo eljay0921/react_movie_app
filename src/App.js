3b9af244009fa825ac71566d7b04e89bf8266e86
jin <lj7812@gmail.com>
2020-07-05 21:05:58 +0900

    # 3.0 Planning the Movie App !
    Movie app 프로젝트 진행에 앞서 이전 내용들을 초기화 했음.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,20 @@
 import React from "react";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log("constructor !");
-  }
-
   state = {
-    count: 3,
-    name: "jin",
-  };
-
-  add = () => {
-    this.setState((current) => ({ count: current.count + 1 }));
-    //console.log(this.state);
-  };
-
-  minus = () => {
-    this.setState((current) => ({ count: current.count - 1 }));
-    //console.log(this.state);
+    isLoading: true,
+    movies: [],
   };
 
   componentDidMount() {
-    console.log("component mounted..!");
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 3000);
   }
 
   render() {
-    console.log("rendering ~~~");
-    return (
-      <div>
-        <h1>My state is : {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div> {isLoading ? "Loading..." : "We are ready !"}</div>;
   }
 
   componentDidUpdate() {
