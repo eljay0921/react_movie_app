d2af3d0796b4609b6e73b7905f8036d040e4d9ba
jin <lj7812@gmail.com>
2020-07-06 22:43:07 +0900

    Updates

--- precache-manifest.a3e3c958047c1ba8bc5bdb949c1cb757.js
+++ precache-manifest.a3e3c958047c1ba8bc5bdb949c1cb757.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d74c0cbfcc493636791107131e505c82",
+    "revision": "3de860c0a905a73dd254dfa98dd33a3d",
     "url": "/react_movie_app/index.html"
   },
   {
-    "revision": "f627017a634e3c972769",
+    "revision": "78458eeb06f15451c749",
     "url": "/react_movie_app/static/css/main.3e00e05e.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react_movie_app/static/js/2.087c2ab0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f627017a634e3c972769",
-    "url": "/react_movie_app/static/js/main.8e16c17a.chunk.js"
+    "revision": "78458eeb06f15451c749",
+    "url": "/react_movie_app/static/js/main.1ece8310.chunk.js"
   },
   {
     "revision": "b2eab6cde90deae0fa53",
