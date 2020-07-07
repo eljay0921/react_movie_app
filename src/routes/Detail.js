1a214899a2836c333c7e0b8d7abd1e31ce63c0b0
jin <lj7812@gmail.com>
2020-07-07 21:22:45 +0900

    # 6.1 BONUS - Movie detail page (2)

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,8 +1,22 @@
 import React from "react";
 
-function Detail(props) {
-  console.log(props);
-  return <span>oh.. ?</span>;
+class Detail extends React.Component {
+  componentDidMount() {
+    let { location, history } = this.props; // props에서 하위의 location과 history 정보만 추출
+    if (location.state === undefined) {
+      // redirect
+      history.push("/");
+    }
+  }
+
+  render() {
+    let { location } = this.props; // props에서 하위의 location 정보만 추출
+    if (location.state) {
+      return <span>{location.state.title}</span>;
+    } else {
+      return null;
+    }
+  }
 }
 
 export default Detail;
