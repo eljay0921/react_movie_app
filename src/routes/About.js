0abd34b5decf5746e601287d54d3dc90c5919a6f
jin <lj7812@gmail.com>
2020-07-07 21:11:03 +0900

    # 6.1 BONUS - Movie detail page

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,10 +1,12 @@
 import React from "react";
+import "./About.css";
 
 function About(props) {
+  console.log(props);
   return (
     <div className="about__container">
-      <span>...!</span>
-      <span>~!~!~!</span>
+      <span>...!ㄴㅁㅇㄹㅁㄴㅇㄹㅁㄴㅇㅎㅁㄴㅇㅎㅁㄴㅎㄴㅁㅎㅁㄴㅇㅎ</span>
+      <span>~!~!~!ㄴㅇㄹㄴㅁㅇㄹㅁㅇㄴㄹㅁㄴㅇ</span>
     </div>
   );
 }
---
+++ src/routes/Detail.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Detail(props) {
+  console.log(props);
+  return <span>oh.. ?</span>;
+}
+
+export default Detail;
